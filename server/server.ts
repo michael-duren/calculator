a6107f88deb03baaf0c42d6c565709727cb60424
Michael Duren <michaeld@michaelduren.com>
2023-05-13 16:16:32 -0500

    somehow lost .tsconfig earlier

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 let equationArray = [];
 let result: number;
@@ -12,7 +12,7 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(express.static('server/public/'));
 
-app.post('/calc', (req, res) => {
+app.post('/calc', (req: Request, res: Response) => {
   equationArray = req.body;
   let parsedEquation = '';
   for (let item of equationArray) {
@@ -32,7 +32,7 @@ app.post('/calc', (req, res) => {
   console.log(result);
 });
 
-app.get('/calc', (req, res) => {
+app.get('/calc', (req: Request, res: Response) => {
   res.send(JSON.stringify(result));
 });
 
