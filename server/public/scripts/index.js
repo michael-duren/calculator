2947055519811a0eb6b4efb3df2b27960d3fcf95
Michael Duren <michaeld@michaelduren.com>
2023-05-13 13:34:13 -0500

    figured out sending equation to server and parsing into a readable equation

--- server/public/scripts/index.js
+++ server/public/scripts/index.js
@@ -1,1 +1,40 @@
 console.log("Hey there buddy I'm not your pal, guy");
+
+// global
+const entryId = document.querySelector('#entry');
+
+let calculation = [];
+
+function addToOperation(event) {
+  const number = event.target.outerText;
+  if (entryId.classList.contains('entry-empty')) {
+    entryId.innerHTML = '';
+    entryId.classList.remove('entry-empty');
+  }
+
+  if (
+    (isNaN(Number(number)) || isNaN(Number(entryId.innerText))) &&
+    number !== '.'
+  ) {
+    entryId.innerHTML = '';
+  }
+
+  entryId.innerHTML += number;
+  calculation.push(number);
+}
+
+function clearOperation() {
+  entryId.innerHTML = '0';
+  calculation = [];
+  entryId.classList.add('entry-empty');
+}
+
+function submitOperation() {
+  fetch('/calc', {
+    method: 'POST',
+    body: JSON.stringify(calculation),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+let equation = [];
+
 const app = express();
 const PORT = 5000;
 
@@ -8,3 +10,18 @@ app.use(express.json());
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(express.static('server/public/'));
+
+app.post('/calc', (req, res) => {
+  equation = req.body;
+  let parsedEquation = '';
+  for (let item of equation) {
+    if (isNaN(Number(item)) && item !== '.') {
+      parsedEquation += ` ${item} `;
+    } else {
+      parsedEquation += item;
+    }
+  }
+
+  res.sendStatus(201);
+  console.log(parsedEquation);
+});
